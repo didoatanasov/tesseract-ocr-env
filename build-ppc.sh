a7ce05138a1c3bb824cda5631f518a7e84323ea4
Deyan Atanasov <dido@omisoft.eu>
2020-01-24 15:44:32 +0200

    Added ppc64le architecture

--- build-ppc.sh
+++ build-ppc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login registry.apps.ocp.ibsbg.bg
-docker build . --platform=ppc64le -t docrec/tesseract-ppc64le:1.0.0
+docker build . --platform=linux/ppc64le --no-cache -t docrec/tesseract-ppc64le:1.0.0 -f Dockerfile-PPC
 docker tag docrec/tesseract-ppc64le:1.0.0 registry.apps.ocp.ibsbg.bg/docrec/tesseract-ppc64le
 docker push registry.apps.ocp.ibsbg.bg/docrec/tesseract-ppc64le
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login registry.apps.ocp.ibsbg.bg
-docker build . -t docrec/tesseract:1.0.0
+docker build . --no-cache  -t docrec/tesseract:1.0.0
 docker tag docrec/tesseract:1.0.0 registry.apps.ocp.ibsbg.bg/docrec/tesseract
 docker push registry.apps.ocp.ibsbg.bg/docrec/tesseract
